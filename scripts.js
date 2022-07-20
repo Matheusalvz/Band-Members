b8bf70554fb96e0c6012de53e790d28e6de86be9
Matheus <teu5991@gmail.com>
2022-07-20 16:07:46 -0300

    Input do usuário e Two way Data Binding

--- scripts.js
+++ scripts.js
@@ -15,9 +15,17 @@ const handlingForms = {
 
     data() {
         return {
-            members: window.members
+            members: window.members,
+            newMember: {
+
+            }
         }
     },
+    methods: {
+        addMember: function() {
+            this.members.push(this.newMember);
+        }
+    }
 
 };
 
